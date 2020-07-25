92a8711d3b08b737b1a5e524b8c3e6e96483ddf8
kaliangrag <65780997+kaliangrag@users.noreply.github.com>
2020-07-25 22:14:04 +0530

    Added work partners slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -201,3 +201,17 @@ function getIP(json) {
 
 	}
 }
+
+
+// Our Partners
+
+	/**********************/
+	/*	Client carousel   */
+	/**********************/
+	$('.carousel-client').bxSlider({
+		auto: true,
+	    slideWidth: 234,
+	    minSlides: 2,
+	    maxSlides: 5,
+	    controls: false
+	});
